a0b18f4d0d33004da486741d2a1bf16123bac63e
Adarsha A <adarshaadi1997@gmail.com>
2022-10-19 16:05:25 +0530

    Clean-Up Function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,10 +12,14 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() =>{
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    },500);
+    return () =>{                //Returning CleanUp function for SideEffect function
+      clearTimeout(identifier);
+    }; 
   },[ enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
